7905dcdccb9fc28fbee7097db71b4f959874b78a
djangobek <nnjjmmjj8@gmail.com>
2025-02-25 19:09:34 +0500

    first commit

---
+++ kwarizmi__script.js
@@ -0,0 +1,10 @@
+// ... (hozirgi JavaScript kodingiz) ...
+
+window.addEventListener('wheel', function(event) {
+    if (event.deltaY > 0) { // Pastga aylantirish
+      // Keyingi bo'limga o'tish va animatsiyani ishga tushirish
+    } else { // Yuqoriga aylantirish
+      // Oldingi bo'limga o'tish va animatsiyani ishga tushirish
+    }
+  });
+  
